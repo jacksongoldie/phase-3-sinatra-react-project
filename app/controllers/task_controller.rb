4810b68c8b8437e603d646c5abd1a8dabe0cf5b4
Goldie <goldiejackson25@yahoo.com>
2022-01-01 18:04:54 -0600

    endpoints set

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -3,4 +3,15 @@ class TaskController < ApplicationController
         tasks = Task.all
         tasks.to_json
     end
+
+    post '/tasks' do
+        task = Task.create(desc: params[:desc], user_id: params[:user_id])
+        task.to_json
+    end
+
+    delete '/tasks/:id' do
+        task = Task.find(params[:id])
+        task.destroy
+        task.to_json
+    end
 end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,5 +3,16 @@ class UserController < ApplicationController
     get '/users' do
         User.all.to_json
     end
-    
+
+    post '/users' do
+        user = User.create(name: params[:name], img_url: params[:img_url])
+        user.to_json
+    end
+
+    delete '/users/:id' do
+        user = User.find(params[:id])
+        user.destroy
+        user.to_json
+    end
+
 end
